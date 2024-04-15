52470d7bb81a6615f65b1b86b26b9974d9fbaf3c
hollis7 <herryhollis@163.com>
2024-04-15 15:40:26 +0800

    save cipher and add hash function

--- three_operator/save_cipher.py
+++ three_operator/save_cipher.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from seal import *
 
 
@@ -68,43 +70,58 @@ def decrypt_cipher(cipher):
     plain = decryptor.decrypt(cipher)
     result = ckks_encoder.decode(plain)
     return result
+def calculate_hash(cipher,salt):
+    '''
+    由于cipher中存在噪声，每次生成的噪声都可能不同
+    因此，参数cipher确保是相同的，比如存在在文件中，或者及时算及时tcp传输
+    '''
+    cipher_bytes = cipher.to_string()+salt
+    # 计算哈希
+    hash_object = hashlib.sha256(cipher_bytes)
+    cipher_hash = hash_object.hexdigest()
+    return cipher_hash
+
 if __name__ == '__main__':
     context, public_key = load_pub_param()
-    scale = 2.0 ** 40
-    ckks_encoder = CKKSEncoder(context)
-    slot_count = ckks_encoder.slot_count()
-    plain1 = [3.1415926] * slot_count
-    plain2 = [2.3] * slot_count
-    save_cipher(plain1, context, public_key, scale, cipher_name='cipher1.bin')
-    save_cipher(plain2, context, public_key, scale, cipher_name='cipher2.bin')
+    # scale = 2.0 ** 40
+    # ckks_encoder = CKKSEncoder(context)
+    # slot_count = ckks_encoder.slot_count()
+    # plain1 = [3.1415926] * slot_count
+    # plain2 = [2.3] * slot_count
+    # save_cipher(plain1, context, public_key, scale, cipher_name='cipher1.bin')
+    # save_cipher(plain2, context, public_key, scale, cipher_name='cipher2.bin')
 
     # 云端加载密文
     cipher1 = load_cipher(context, cipher_name='cipher1.bin')
     cipher2 = load_cipher(context, cipher_name='cipher2.bin')
+    # 密文hash测试
+    salt= b'random salt'
+    sha= calculate_hash(cipher1,salt)
+    print(sha)
 
-    evaluator = Evaluator(context)
-    add_result = evaluator.add(cipher1, cipher2)
-    mul_result = evaluator.multiply(cipher1, cipher2)
-    sub_result = evaluator.sub(cipher1, cipher2)
+    # evaluator = Evaluator(context)
+    # add_result = evaluator.add(cipher1, cipher2)
+    # mul_result = evaluator.multiply(cipher1, cipher2)
+    # sub_result = evaluator.sub(cipher1, cipher2)
 
     # 边缘节点解密
-    context, public_key, secret_key = load_all_param()
-    ckks_encoder = CKKSEncoder(context)
-    slot_count = ckks_encoder.slot_count()
-    decryptor = Decryptor(context, secret_key)
-
-    add_plain = decryptor.decrypt(add_result)
-    add_plain = ckks_encoder.decode(add_plain)
-
-    mul_plain = decryptor.decrypt(mul_result)
-    mul_plain = ckks_encoder.decode(mul_plain)
-
-    sub_plain = decryptor.decrypt(sub_result)
-    sub_plain = ckks_encoder.decode(sub_plain)
-
-    print("add_plain:  ")
-    print(add_plain)
-    print("mul_plain:  ")
-    print(mul_plain)
-    print("sub_plain:  ")
-    print(sub_plain)
+    # context, public_key, secret_key = load_all_param()
+    # ckks_encoder = CKKSEncoder(context)
+    # slot_count = ckks_encoder.slot_count()
+    # decryptor = Decryptor(context, secret_key)
+    #
+    # add_plain = decryptor.decrypt(add_result)
+    # add_plain = ckks_encoder.decode(add_plain)
+    #
+    # mul_plain = decryptor.decrypt(mul_result)
+    # mul_plain = ckks_encoder.decode(mul_plain)
+    #
+    # sub_plain = decryptor.decrypt(sub_result)
+    # sub_plain = ckks_encoder.decode(sub_plain)
+    #
+    # print("add_plain:  ")
+    # print(add_plain)
+    # print("mul_plain:  ")
+    # print(mul_plain)
+    # print("sub_plain:  ")
+    # print(sub_plain)
